5e3992e6b48871d1d2fc6746ce88af2d703c2b43
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-03-23 11:42:14 +0100

    edit sign-in html

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
         <form>
             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <input type="email" className="form-control"  placeholder="name@example.com" />
 
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <input type="password" className="form-control"  placeholder="Password" />
 
             <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
         </form>
