a5fca53117ad47e626f92fa76724bcea3278fd11
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-03-23 14:15:10 +0100

    refactoring HTML

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,16 @@ function App() {
     <div className="App">
         <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Top navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarCollapse">
+    <a className="navbar-brand" href="#">Home</a>
+    <div>
       <ul className="navbar-nav me-auto mb-2 mb-md-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">Login</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a className="nav-link" href="#">Register</a>
         </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
