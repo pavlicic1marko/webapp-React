4d4dfdda284c3d8896515e45fa0ee73bc9b408df
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-03-23 11:52:49 +0100

    edit sign-in html, add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Top navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarCollapse">
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
       <main className="form-signin w-100 m-auto">
         <form>
             <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
