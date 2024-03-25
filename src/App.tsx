26db07dd7da045b003f233cdcfadb2a6e4eed2cf
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-03-25 15:47:34 +0100

    adding register and Home pages, and adding 'react router dom'

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./pages/Login";
-import Nav from "./components/Nav"
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import {BrowserRouter , Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <Nav/>
-        <main className="form-signin w-100 m-auto">
-            <Login />
-        </main>
+        <BrowserRouter>
+            <Nav/>
+            <main className="form-signin w-100 m-auto">
+                <Route path='/' exact component ={Home}/>
+                <Route path='/login' component ={Login}/>
+                <Route path='/register' component ={Register}/>
+
+            </main>
+        </BrowserRouter>
+
     </div>
+
   );
 }
 
