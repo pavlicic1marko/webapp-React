de4c377c67c963d9ed241e2fe7446180a6423f56
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-03-25 16:06:20 +0100

    adding working links to Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
     const Nav = () => {
         return(
-        <div>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Home</a>
-                <div>
+                    <Link to='/' className="navbar-brand">Home</Link>
+                </div>
+
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Login</a>
+                    <Link to='/Login' className="nav-link active" aria-current="page" >Login</Link>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Register</a>
+                    <Link to='/Register' className="nav-link" >Register</Link>
                 </li>
             </ul>
-            </div>
-             </div>
             </nav>
-        </div>
         );
     };
 
